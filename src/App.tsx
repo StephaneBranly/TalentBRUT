8e0810f5b9d2d1dd15e8f28cf3b819b365cc549f
Stephane Branly <stephanebranly.pro@gmail.com>
2020-11-25 22:57:02 +0100

    add: first packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#05b592",
+    },
+    secondary: {
+      main: "#f2c7f2",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Button variant="contained">Talent Br'UT</Button>
+    </ThemeProvider>
   );
 }
 

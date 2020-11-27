fb24b3868d25529b2b1d7a09174fb26219c95167
Stephane Branly <stephanebranly.pro@gmail.com>
2020-11-27 17:57:38 +0100

    add: email form

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -18,8 +18,66 @@ import {
 } from "@material-ui/core";
 import { Favorite, Send, ThumbUp } from "@material-ui/icons";
 
-export class Contact extends Component {
+export interface ContactProps {}
+export interface ContactState {
+  object: string;
+  email: string;
+  message: string;
+  tag: string;
+}
+export class Contact extends Component<ContactProps, ContactState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      object: "",
+      email: "",
+      message: "",
+      tag: "",
+    };
+    this.onChangeObject = this.onChangeObject.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangeMessage = this.onChangeMessage.bind(this);
+    this.onChangeTag = this.onChangeTag.bind(this);
+  }
+  onChangeObject(event: any) {
+    this.setState({
+      object: event.target.value,
+    });
+  }
+  onChangeEmail(event: any) {
+    this.setState({
+      email: event.target.value,
+    });
+  }
+  onChangeMessage(event: any) {
+    this.setState({
+      message: event.target.value,
+    });
+  }
+  onChangeTag(event: any) {
+    this.setState({
+      tag: event.target.value,
+    });
+  }
+  sendEmail = () => {
+    const { object, message, tag, email } = this.state;
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        from: email,
+        message: message,
+        tag: tag,
+        object: object,
+      }),
+    };
+    fetch("https://assos.utc.fr/talentbrut/server/api/mail.php", requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   render() {
+    const { object, message, tag, email } = this.state;
     return (
       <Card>
         <CardActionArea>
@@ -40,29 +98,41 @@ export class Contact extends Component {
             >
               <Grid item>
                 <FormControl fullWidth>
-                  <InputLabel htmlFor="why">Objet du contact</InputLabel>
-                  <Select label="WHY">
-                    <MenuItem value={10}>Sponsoring</MenuItem>
-                    <MenuItem value={20}>Talent</MenuItem>
-                    <MenuItem value={30}>Communication</MenuItem>
-                    <MenuItem value={40}>Teams</MenuItem>
-                    <MenuItem value={50}>Autre</MenuItem>
+                  <InputLabel htmlFor="tag">Objet du contact</InputLabel>
+                  <Select
+                    id="tag"
+                    label="tag"
+                    value={tag}
+                    onChange={(e) => this.onChangeTag(e)}
+                  >
+                    <MenuItem value={"Sponsoring"}>Sponsoring</MenuItem>
+                    <MenuItem value={"Talent"}>Talent</MenuItem>
+                    <MenuItem value={"Communication"}>Communication</MenuItem>
+                    <MenuItem value={"Teams"}>Teams</MenuItem>
+                    <MenuItem value={"Autre"}>Autre</MenuItem>
                   </Select>
                 </FormControl>
               </Grid>
 
               <Grid item>
                 <FormControl fullWidth>
-                  <TextField id="standard-basic" label="Titre" />
+                  <TextField
+                    id="object"
+                    label="Titre"
+                    value={object}
+                    onChange={(e) => this.onChangeObject(e)}
+                  />
                 </FormControl>
               </Grid>
 
               <Grid item>
                 <FormControl fullWidth>
                   <TextField
-                    id="standard-basic"
+                    id="email"
                     label="Votre email"
                     type="email"
+                    value={email}
+                    onChange={(e) => this.onChangeEmail(e)}
                   />
                 </FormControl>
               </Grid>
@@ -70,14 +140,21 @@ export class Contact extends Component {
               <Grid item>
                 <FormControl fullWidth>
                   <TextField
-                    id="standard-basic"
+                    id="message"
                     multiline
                     label="Description"
+                    value={message}
+                    onChange={(e) => this.onChangeMessage(e)}
                   />
                 </FormControl>
               </Grid>
               <Grid item>
-                <Button variant="contained" color="primary" endIcon={<Send />}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  endIcon={<Send />}
+                  onClick={() => this.sendEmail()}
+                >
                   Envoyer
                 </Button>
               </Grid>
